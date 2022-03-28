82312fb9ffa9fd976a62038ac03b99c12dc8506b
kawsar <kawsarfaz100@gmail.com>
2022-03-28 12:22:40 +0600

    banner show done

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     use HasFactory;
+
+    public static function getBanners()
+    {
+        $getBanners = Banner::where('status',1)->get();
+        return $getBanners;
+    }
 }
